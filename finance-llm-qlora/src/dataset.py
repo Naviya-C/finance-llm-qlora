5df5543c3f9faf9e55b048a8c5044be8469e7b54
Naveen <naviyac491@gmail.com>
2026-03-19 18:53:46 +0530

    change format function -> format-entry cause name conflict with python built-in

--- finance-llm-qlora/src/dataset.py
+++ finance-llm-qlora/src/dataset.py
@@ -2,11 +2,11 @@ from datasets import load_dataset
 
 dataset = load_dataset("FinGPT/fingpt-fiqa_qa")
 
-def format(entry):
+def format_entry(entry):
     sys_promt = "You are a helpful, professional financial assistant."
     
     instruction = entry['instruction']
-    input = entry['input']
+    input = entry['input'] if entry['input'] else ""
     output = entry['output']
     
     format_text = f"<s>[INST]<<SYS>>\n{sys_promt}\n<</SYS>>\n{instruction}\n{input}[/INST]{output}</s>"
@@ -14,6 +14,6 @@ def format(entry):
     return {"text": format_text}
 
 def prepare_dataset(data):
-    format_dataset = data['train'].map(format)
+    format_dataset = data['train'].map(format_entry)
     return format_dataset
 
